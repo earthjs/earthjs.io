842013e540014d83da60111ef9fe4910d2e26fce
wharsojo <wharsojo@gmail.com>
2017-07-13 08:59:54 +0800

    update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
     <div id="app-eq">
         <div class="set-options"></div>
-        <svg id="earth"></svg>
-        <canvas class="canvas"></canvas>
+        <svg id="earth" class="ea-layer ea-1"></svg>
+        <div class="c-canvas">
+            <canvas  class="canvas ea-layer ea-2"></canvas>
+            <canvas  class="canvas ea-layer ea-3"></canvas>
+        </div>
         <div class="set-options2">
-            <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="6"> mag:6</input></label>
-            <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="5"> mag:5</input></label>
-            <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="4"> mag:4</input></label>
-            <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="3"> mag:3</input></label>
-            <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="2"> mag:2</input></label>
         </div>
         <script type='text/javascript' src="./ex/index.js"></script>
     </div>
@@ -25,11 +23,22 @@ export default {
         v: '3'
     },
 }
+// <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="6"> mag:6</input></label>
+// <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="5"> mag:5</input></label>
+// <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="4"> mag:4</input></label>
+// <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="3"> mag:3</input></label>
+// <label><input @click="mag" type="radio" name="magnitue" v-model="v" value="2"> mag:2</input></label>
 </script>
 
 <style>
-#earth, .canvas {
+#earth, .c-canvas {
     left: -200px;
+}
+.c-canvas {
+    position: absolute;
+}
+input[type="range"] {
+    padding-bottom: 5px;
 }
 .top {
     padding: 10px 0 0 15px;
